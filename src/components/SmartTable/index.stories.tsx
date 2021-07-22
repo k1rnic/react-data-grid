c84a94c4e83621fe8030af5d33cd4e0b6eb579b7
k1rnic <knikolskiy.io@gmail.com>
2021-07-22 18:41:45 +0300

    refactor: remove unnecessory hooks and code

--- src/components/SmartTable/index.stories.tsx
+++ src/components/SmartTable/index.stories.tsx
@@ -1,11 +1,14 @@
 import { Meta, Story } from '@storybook/react';
 import React from 'react';
-import usePokedex from '../../hooks/usePokedex';
 import SmartTable, { SmartTableProps } from './';
 
 const columns = [
-  { name: 'id', title: 'ID' },
-  { name: 'name', title: 'Name' },
+  { name: '1', title: 'Column1' },
+  { name: '2', title: 'Column2' },
+  { name: '3', title: 'Column3' },
+  { name: '4', title: 'Column4' },
+  { name: '5', title: 'Column5' },
+  { name: '6', title: 'Column6' },
 ];
 
 export default {
@@ -14,9 +17,7 @@ export default {
 } as Meta<SmartTableProps>;
 
 const Template: Story<SmartTableProps> = (args) => {
-  const [{ pokemons }] = usePokedex();
-
-  return <SmartTable {...args} data={pokemons} columns={columns} />;
+  return <SmartTable {...args} data={[]} columns={columns} />;
 };
 
 export const Overview = Template.bind({});
