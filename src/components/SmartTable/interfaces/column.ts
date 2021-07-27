b0c503259e9363ede9a35cb881afe2ac92689157
k1rnic <knikolskiy.io@gmail.com>
2021-07-27 18:48:40 +0300

    feat(SmartTable/Column): add type prop

--- src/components/SmartTable/interfaces/column.ts
+++ src/components/SmartTable/interfaces/column.ts
@@ -1,6 +1,14 @@
 import { GridProps } from '@devexpress/dx-react-grid-material-ui';
 
+export type SmartTableColumnType =
+  | 'number'
+  | 'text'
+  | 'date'
+  | 'datetime'
+  | 'time';
+
 export type SmartTableColumn<T> = {
   name: keyof T;
+  type?: SmartTableColumnType;
   getCellValue?: (row: T, columnName: keyof T) => any;
 } & GridProps['columns'][number];
